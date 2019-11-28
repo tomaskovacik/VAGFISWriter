2519f446f6cda662962283163ec1926be1689ca0
Tomas kovacik <nail@nodomain.sk>
2019-11-28 13:02:34 +0100

    example with more exmaples :)

--- examples/b5_fun/b5_fun.ino
+++ examples/b5_fun/b5_fun.ino
@@ -67,6 +67,7 @@ void redrawFrameBuffer() {
 void setup() {
   fisWriter.begin();
   fisWriter.reset();
+  Serial.begin(115200);
 }
 
 
@@ -74,12 +75,20 @@ void loop() {
   fisWriter.initFullScreen();
   fisWriter.GraphicFromArray(0, 0, 64, 88, b5f, 1);
   delay(3000);
+  //inverted
+//  fisWriter.initFullScreenFilled();
+  //draw mode = 1, only 0 from data are used 
+//  fisWriter.GraphicFromArray(0, 0, 64, 88, b5f, 0);
+//  delay(3000);
   fisWriter.initFullScreenFilled();
-  fisWriter.GraphicFromArray(0, 0, 64, 88, b5f, 0);
+  //draw mode = 1, only 0 from data are used 
+  fisWriter.GraphicFromArray(0, 0, 64, 88, b5f, 1);
+  delay(1000);
+  fisWriter.GraphicFromArray(0, 0, 64, 88, b5f, 2);
   delay(3000);
   fisWriter.initFullScreen();
-  fisWriter.GraphicFromArray(0, 0, 64, 65, Q, 1);
-  fisWriter.GraphicFromArray(0, 70, 64, 16, QBSW, 1);
+  fisWriter.GraphicFromArray(0, 0, 64, 65, Q, 2);
+  fisWriter.GraphicFromArray(0, 70, 64, 16, QBSW, 2);
   delay(3000);
   fisWriter.reset();
   fisWriter.sendMsg("12345678  TEST  ");
@@ -100,6 +109,23 @@ void loop() {
   }
   fisWriter.sendMsg("  TEST  12345678");
   delay(1000);
+  fisWriter.initFullScreen();
+
+  fisWriter.sendMsgFS(0,0,0x00,6,"LINE 1"); // 0x00 - standard font, negative, left aligned
+  fisWriter.sendMsgFS(0,8,0x01,6,"LINE 2"); // 0x01 - standard font, positive, left aligned
+  fisWriter.sendMsgFS(0,16,0x04,6,"LINE 3"); // 0x04 - compressed font, negative, left aligned
+  fisWriter.sendMsgFS(0,24,0x05,6,"LINE 4"); // 0x05 - compressed font, positive, left-aligned
+  fisWriter.sendMsgFS(0,32,0x08,6,"LINE 5"); // 0x08 - special characters, negative, left aligned
+  fisWriter.sendMsgFS(0,40,0x09,6,"LINE 6"); // 0x09 - special characters, positive, left aligned
+  fisWriter.sendMsgFS(0,48,0x20,6,"LINE 7"); // 0x20 - standard font, negative, centered
+  fisWriter.sendMsgFS(0,56,0x21,6,"LINE 8"); // 0x21 - standard font, positive, centered
+  fisWriter.sendMsgFS(0,64,0x24,6,"LINE 9"); // 0x24 - compressed font, negative, centered
+  fisWriter.sendMsgFS(0,72,0x25,13,"1234567891011"); // 0x25 - compressed font, positive, centered
+  fisWriter.sendMsgFS(0,80,0x28,13,"1234567891011"); // 0x28 - special characters, positive, centered
+  delay(5000);
+  fisWriter.initFullScreen();
+  fisWriter.sendMsgFS(0,0,0x29,7,"LINE 1"); // 0x29 - special characters, negative, centered
+  delay(5000);
   //while(true){ //and display will did not change ever
   //  fisWriter.sendKeepAliveMsg();
   //  delay(1000);
